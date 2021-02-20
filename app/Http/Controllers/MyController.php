9bfa3b84ee72dc7815196d5c18547526fe44daad
andreabecerrab <andii1wiki@gmail.com>
2021-02-19 19:19:19 -0600

    commit inicial

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+
+class MyController extends Controller
+{
+    public function index(){
+        return view('mi-clase');
+    }
+}
