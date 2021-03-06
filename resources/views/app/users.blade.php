a68f4570f9351de43d46f104dfe77cefd466583f
andreabecerrab <andii1wiki@gmail.com>
2021-03-06 14:07:26 -0600

    delete users

--- resources/views/app/users.blade.php
+++ resources/views/app/users.blade.php
@@ -8,7 +8,9 @@
         <tr>
             <th>Name</th>
             <th>Email</th>
+            @if (Auth::user()->role_id === 4)
             <th>Delete</th>
+            @endif
         </tr>
     </thead>
     <tbody>
@@ -16,15 +18,19 @@
             <tr>
                 <td>{{ $item->name }}</td>
                 <td>{{ $item->email }}</td>
+                @auth
+                @if (Auth::user()->role_id === 4)
                 <td>
                     <form action="{{ route('users.destroy', ['user' => $item]) }}" method="post">
                         @csrf
                         @method('DELETE')
                         <input type="submit" value="delete" class="btn btn-danger">
-
                         </input>
                     </form>
                 </td>
+                @endif
+                @endauth
+
             </tr>
         @endforeach
     </tbody>
